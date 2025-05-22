277b300d43c366900782bf1b9ec1bd5e1eff6c02
munawwarkp <munaaawarrkp@gmail.com>
2025-05-22 10:17:02 +0530

    Time: 1 ms (45.73%), Space: 54.4 MB (29.88%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+   var count=0;
+    var res = 0;
+
+  for(i=0;i<s.length;i++){
+    if(s.charAt(i)=='('){
+        count++
+         if(count>res){
+            res=count
+        }
+        
+    }
+    else if(s.charAt(i) == ')'){
+        count--
+       
+    }
+  }
+  return res
+
+};
