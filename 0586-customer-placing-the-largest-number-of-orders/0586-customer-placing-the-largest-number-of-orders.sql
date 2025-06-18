e3214e721abd5e5e6375ab515e47d9bb4bf71b40
munawwarkp <munaaawarrkp@gmail.com>
2025-06-18 10:29:05 +0530

    Time: 551 ms (26.11%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,9 +1,15 @@
 /* Write your T-SQL query statement below */
-with highestOrder as
-(
-    select customer_number,count(1) as count_num from Orders
-    group by(customer_number)
-)
-select customer_number from highestOrder
-order by count_num desc
+-- with highestOrder as
+-- (
+--     select customer_number,count(1) as count_num from Orders
+--     group by(customer_number)
+-- )
+-- select customer_number from highestOrder
+-- order by count_num desc
+-- offset 0 rows fetch next 1 row only
+
+
+select customer_number from Orders
+group by customer_number
+order by count(1) desc
 offset 0 rows fetch next 1 row only
