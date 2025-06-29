b76f5ff74f98b46c62f3c4baa51bf8805441ba7c
munawwarkp <munaaawarrkp@gmail.com>
2025-06-29 19:21:52 +0530

    Time: 607 ms (15.1%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-
-select w2.id from Weather w1
-join Weather w2
-on  w2.recordDate = DATE_ADD(w1.recordDate,interval 1 day) and w2.temperature > w1.temperature
+/* Write your T-SQL query statement below */
+select yesterday.id from  Weather yesterday
+join Weather today
+on datediff(DAY,today.recordDate,yesterday.recordDate) = 1 
+where yesterday.temperature>today.temperature
