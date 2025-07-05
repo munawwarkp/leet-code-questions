0e7ec6cc2bf582721f7d2b5f61475369b5529651
munawwarkp <munaaawarrkp@gmail.com>
2025-07-05 11:25:56 +0530

    Time: 427 ms (57.44%), Space: 0B (100%) - LeetHub

---
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+select Students.student_id, Students.student_name,Subjects.subject_name,COUNT(Examinations.student_id) as attended_exams from Students
+cross join Subjects
+left join Examinations
+on Students.student_id=Examinations.student_id and Subjects.subject_name=Examinations.subject_name
+group by Students.student_id,Students.student_name,Subjects.subject_name
