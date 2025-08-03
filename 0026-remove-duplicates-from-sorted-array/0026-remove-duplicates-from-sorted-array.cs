481ce0019694483acfd8f3ecc9bcaf3958b93d4d
munawwarkp <munaaawarrkp@gmail.com>
2025-08-03 09:12:37 +0530

    Time: 0 ms (100%), Space: 50.9 MB (88.86%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,21 +1,41 @@
 public class Solution {
-    public int RemoveDuplicates(int[] nums) {
-       int[] newArr = new int[] {};
+//     public int RemoveDuplicates(int[] nums) {
+//        int[] newArr = new int[] {};
 
- foreach(int i in nums)
+//  foreach(int i in nums)
+//  {
+//      if (!newArr.Contains(i))
+//      {
+//        newArr =  newArr.Concat(new int[] { i }).ToArray();
+//      }
+
+//  }
+//   for(int i = 0; i < newArr.Length; i++)
+//  {
+//      nums[i] = newArr[i];
+//      Console.WriteLine(i);
+//  }
+       
+//  return newArr.Length;
+//     }
+
+
+ public int RemoveDuplicates(int[] nums)
  {
-     if (!newArr.Contains(i))
+
+     int index = 1;
+
+     for(int i = 0; i < nums.Length; i++)
      {
-       newArr =  newArr.Concat(new int[] { i }).ToArray();
+         if (nums[i] != nums[index - 1])
+         {
+             nums[index] = nums[i];
+             index++;
+         }
      }
 
+     return index;
+
  }
-  for(int i = 0; i < newArr.Length; i++)
- {
-     nums[i] = newArr[i];
-     Console.WriteLine(i);
- }
-       
- return newArr.Length;
-    }
+
 }
