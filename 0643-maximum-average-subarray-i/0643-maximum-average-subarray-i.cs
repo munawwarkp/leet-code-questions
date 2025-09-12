fac5328051be76f099094e22c89b50277edc00cf
munawwarkp <munaaawarrkp@gmail.com>
2025-09-12 23:36:18 +0530

    Time: 2 ms (100%), Space: 60.7 MB (21.54%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cs
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cs
@@ -1,23 +1,48 @@
 public class Solution {
-    public double FindMaxAverage(int[] nums, int k) {
-    double maxAverage = 0;
-    int lastSum = int.MinValue;
-    int sum = 0;
+    // public double FindMaxAverage(int[] nums, int k) {
+    // double maxAverage = 0;
+    // int lastSum = int.MinValue;
+    // int sum = 0;
 
 
-        for (int i = 0; i<= nums.Length-k; i++)
-        {
-            sum = 0;
-            for (int j = i; j < k+i; j++)
-            {
-                sum += nums[j];
+    //     for (int i = 0; i<= nums.Length-k; i++)
+    //     {
+    //         sum = 0;
+    //         for (int j = i; j < k+i; j++)
+    //         {
+    //             sum += nums[j];
                 
-            }
-            if(sum > lastSum)
-                lastSum = sum;
+    //         }
+    //         if(sum > lastSum)
+    //             lastSum = sum;
 
-        }
-    maxAverage = Convert.ToDouble(lastSum)/k;
-    return maxAverage;
-    }
+    //     }
+    // maxAverage = Convert.ToDouble(lastSum)/k;
+    // return maxAverage;
+    // }
+
+
+    //using sliding window
+     public double FindMaxAverage(int[] nums, int k)
+ {
+     int sum = 0;
+
+     for(int i=0; i<k; i++)
+     {
+         sum += nums[i];
+     }
+
+     int maxSum = sum;
+
+     for(int i=k; i<nums.Length; i++)
+     {
+         sum += nums[i] - nums[i-k];
+         if(sum > maxSum)
+             maxSum = sum;
+
+     }
+     return (double)maxSum/k;
+
+ }
 }
+
