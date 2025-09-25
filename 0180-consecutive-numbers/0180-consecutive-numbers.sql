3067e64302ace38144f3daf7b57d4450386b173c
munawwarkp <munaaawarrkp@gmail.com>
2025-09-25 10:53:06 +0530

    Time: 546 ms (69.9%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+select 
+    distinct a.num as ConsecutiveNums 
+from Logs a
+join Logs b on a.id = b.id + 1
+join Logs c on b.id+1 = c.id - 1
+where a.num=b.num and a.num = c.num
