5eb10cf498a7b93ec2cd7057c42e9d147808c949
munawwarkp <munaaawarrkp@gmail.com>
2025-10-14 10:17:50 +0530

    Time: 11039 ms (65.26%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,27 +1,18 @@
 /* Write your T-SQL query statement below */
 select 
 	'Low Salary' as category,
-	COUNT(
-	case
-		when income < 20000 then 1
-	end
-	) as accounts_count 
+	count(income) as accounts_count
 from Accounts
+where income<20000
 union all
 select 
 	'Average Salary' as category,
-	COUNT(
-	case 
-		when income between 20000 and 50000 then 1
-	end
-	) as accounts_count 
+	count(income) as accounts_count
 from Accounts
-union all
+where income between 20000 and 50000
+union all 
 select 
 	'High Salary' as category,
-	COUNT(
-	case 
-		when income > 50000 then 1
-	end 
-	) as accounts_count 
+	count(income) as accounts_count
 from Accounts
+where income > 50000
