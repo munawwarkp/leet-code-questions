d51413f5475e0d2bfc908966066b526b59ec70c7
munawwarkp <munaaawarrkp@gmail.com>
2025-11-16 19:47:54 +0530

    Time: 36 ms (85.42%), Space: 77.9 MB (47.92%) - LeetHub

--- 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.cs
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.cs
@@ -1,34 +1,17 @@
 public class Solution {
     public int MaxWidthOfVerticalArea(int[][] points) {
         
-          int[]newArr = new int[points.Length];
+       Array.Sort(points, (a,b) => a[0].CompareTo(b[0]));
 
-  for (int i = 0; i < points.Length; i++)
-  {
-      for (int j=0; j < points[i].Length; j++)
-      {
-          if(j == 0)
-          {
-              newArr[i] = points[i][j];
-          }
-      }
-  }
-  Array.Sort(newArr);
-
-  int l = 1;
-  int greatest = 0;
-  for(int i=0;i < newArr.Length-1; i++)
-  {
-      if (newArr[l] - newArr[i] > greatest)
-      {
-
-          greatest = newArr[l] - newArr[i];
-         
-      }
-     
-      l++;
-  }
-
-  return greatest;
+ int greatest = 0;
+ for (int i=0; i< points.Length-1 ; i++)
+ {
+     if(points[i+1][0] - points[i][0] > greatest)
+     {
+        
+         greatest = points[i+1][0] - points[i][0];
+     }
+ }
+ return greatest;
     }
 }
