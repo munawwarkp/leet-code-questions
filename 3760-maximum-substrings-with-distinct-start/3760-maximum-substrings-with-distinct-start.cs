da174e9e18645ee26bec19d5e54edad2c7d20c6f
munawwarkp <munaaawarrkp@gmail.com>
2026-01-07 12:32:30 +0530

    Time: 21 ms (36.57%), Space: 51.1 MB (26.21%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cs
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cs
@@ -1,15 +1,11 @@
 public class Solution {
     public int MaxDistinct(string s) {
         
-        Hashtable ht = new Hashtable();
-        int count = 0;
+       HashSet<char> hs = new HashSet<char>();
+       for(int i=0; i<s.Length; i++){
+            hs.Add(s[i]);
+       }
 
-        for(int i=0; i<s.Length; i++){
-            if(!ht.ContainsKey(s[i])){
-                ht[s[i]] = count++;
-            }
-        }
-
-        return count;
+       return hs.Count;
     }
 }
