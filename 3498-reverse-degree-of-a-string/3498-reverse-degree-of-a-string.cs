df9bb2c7ff72d1204b16fb09abee915b533aa43c
munawwarkp <munaaawarrkp@gmail.com>
2026-03-01 13:48:10 +0530

    Time: 1 ms (100%), Space: 42.2 MB (62.2%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cs
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cs
@@ -1,25 +1,35 @@
 public class Solution {
     public int ReverseDegree(string s) {
         
-Dictionary<char, int> charIndex = new Dictionary<char, int>();
-int aAsciVal = Convert.ToInt32('a');
+
+int sum = 0;
 int zAsciVal = Convert.ToInt32('z');
-int position = 26;
 
-while (aAsciVal <= zAsciVal)
+for (int i = 0; i<s.Length; i++)
 {
-    charIndex[Convert.ToChar(aAsciVal)] = position;
-    position--;
-    aAsciVal++;
-}
-
-int sum = 0;    
-
-for (int i=0; i< s.Length; i++)
-{
-    charIndex.TryGetValue(s[i], out int index);
-    sum += index * (i + 1);
+    sum += ((zAsciVal+1) - Convert.ToInt32(s[i])) * (i+1);
 }
 return sum;
     }
+// Dictionary<char, int> charIndex = new Dictionary<char, int>();
+// int aAsciVal = Convert.ToInt32('a');
+// int zAsciVal = Convert.ToInt32('z');
+// int position = 26;
+
+// while (aAsciVal <= zAsciVal)
+// {
+//     charIndex[Convert.ToChar(aAsciVal)] = position;
+//     position--;
+//     aAsciVal++;
+// }
+
+// int sum = 0;    
+
+// for (int i=0; i< s.Length; i++)
+// {
+//     charIndex.TryGetValue(s[i], out int index);
+//     sum += index * (i + 1);
+// }
+// return sum;
+//     }
 }
